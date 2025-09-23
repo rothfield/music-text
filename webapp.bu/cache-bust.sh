aa643d6980afd47adf577a64fbb7614afe8185f3
John Rothfield <johnrothfield@gmail.com>
2025-09-23 06:01:39 -0600

    cleanup

--- webapp.bu/cache-bust.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Update cache-busting versions for web assets
-
-cd "$(dirname "$0")"
-node update-cache-bust.js
-echo "Cache-busting versions updated. Browsers will reload updated assets."
